54b6b9e43273fba1f27a547ea87f41d03a94782a
Gaurav12342 <gauravpendharkar2@gmail.com>
2022-12-06 00:10:10 +0530

    Added youtube viewr

--- src/Pages/Banner.tsx
+++ src/Pages/Banner.tsx
@@ -1,10 +1,24 @@
 import { FC, useState, useEffect } from "react";
+import { Dialog, DialogContent, Paper, PaperProps } from "@mui/material";
+import Draggable from "react-draggable";
+import ReactPlayer from "react-player";
 import axios from "utils/AxiosInterceptor";
+
+const PaperComponent = (props: PaperProps) => {
+  return (
+    <Draggable
+      handle="#draggable-dialog-title"
+      cancel={'[class*="MuiDialogContent-root"]'}
+    >
+      <Paper {...props} />
+    </Draggable>
+  );
+};
 
 const Banner: FC = () => {
   const [bannerData, setBannerData] = useState<any>({});
-
-  console.log("bannerData", bannerData);
+  const [open, setOpen] = useState(false);
+  const [videoUrl, setVideoUrl] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -25,6 +39,25 @@ const Banner: FC = () => {
       .catch();
   };
 
+  const handleDialog = (value: any) => {
+    setOpen(false);
+    axios
+      .get(`tv/${value}/videos`)
+      .then((data: any) => {
+        if (data?.status === 200) {
+          const url = `https://www.youtube.com/watch?v=${data?.data?.results[0]?.key}`;
+          setVideoUrl(url);
+          setOpen(true);
+        }
+      })
+      .catch();
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setVideoUrl("");
+  };
+
   return (
     <div>
       <div className="text-center text-yellow-600 mb-10 relative">
@@ -43,7 +76,10 @@ const Banner: FC = () => {
           </p>
 
           <div className="flex">
-            <button className="block text-sm text-white mr-4 py-2 px-4 rounded-md border-0 font-semibold bg-slate-400 uppercase">
+            <button
+              className="block text-sm text-white mr-4 py-2 px-4 rounded-md border-0 font-semibold bg-slate-400 uppercase"
+              onClick={() => handleDialog(bannerData?.id)}
+            >
               Play
             </button>
             <button className="block text-sm text-white mr-4 py-2 px-4 rounded-md border-0 font-semibold bg-slate-400 uppercase">
@@ -56,6 +92,25 @@ const Banner: FC = () => {
           </p>
         </div>
       </div>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperComponent={PaperComponent}
+        fullWidth={true}
+        maxWidth={"md"}
+        aria-labelledby="draggable-dialog-title"
+      >
+        <DialogContent>
+          <ReactPlayer
+            muted={true}
+            width={"840px"}
+            playing={true}
+            controls={true}
+            url={videoUrl}
+          />
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
