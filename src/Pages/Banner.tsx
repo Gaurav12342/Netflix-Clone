46414c5f80035285041645acfc5f438d978424aa
Gaurav12342 <gauravpendharkar2@gmail.com>
2022-12-05 23:40:54 +0530

    Integrete API in banner component

--- src/Pages/Banner.tsx
+++ src/Pages/Banner.tsx
@@ -1,26 +1,67 @@
-import { FC } from 'react'
+import { FC, useState, useEffect } from "react";
+import axios from "utils/AxiosInterceptor";
 
 const Banner: FC = () => {
-    return (
-        <div>
-            <div className="text-center text-yellow-600 mb-10 relative">
-                <img className='relative w-1/1' src={'https://image.tmdb.org/t/p/original/6ESvUtiCv3XqJRTU9d32I4R0Ckq.jpg'} alt="Freedom Blog" />
-                <div className="absolute top-20 left-10 flex flex-col items-start space-y-8">
-                    <label className='font-bold text-5xl text-white'>
-                        {'Dhanna pani'}
-                    </label>
+  const [bannerData, setBannerData] = useState<any>({});
 
-                    <p className='font-bold text-xl text-white text-start'>
-                        When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.
-                    </p>
+  console.log("bannerData", bannerData);
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-                    <p className='uppercase font-bold text-xl text-white text-start'>
-                        RATING: 8.6/10
-                    </p>
-                </div>
-            </div>
+  const fetchData = () => {
+    axios
+      .get(`/discover/tv?with_networks=213`)
+      .then((response: any) => {
+        if (response?.status === 200) {
+          const resultslength = response?.data?.results;
+          const newIndex = Math.floor(Math.random() * resultslength?.length);
+          resultslength?.length &&
+            resultslength !== NaN &&
+            setBannerData(resultslength[newIndex]);
+        }
+      })
+      .catch();
+  };
+
+  return (
+    <div>
+      <div className="text-center text-yellow-600 mb-10 relative">
+        <img
+          className="relative w-1/1"
+          src={
+            "https://image.tmdb.org/t/p/original/6ESvUtiCv3XqJRTU9d32I4R0Ckq.jpg"
+          }
+          alt="Freedom Blog"
+        />
+        <div className="absolute top-20 left-10 flex flex-col items-start space-y-8">
+          <label className="font-bold text-5xl text-white">
+            {"Dhanna pani"}
+          </label>
+
+          <p className="font-bold text-xl text-white text-start">
+            When a young boy vanishes, a small town uncovers a mystery involving
+            secret experiments, terrifying supernatural forces, and one strange
+            little girl.
+          </p>
+
+          <div className="flex">
+            <button className="block text-sm text-white mr-4 py-2 px-4 rounded-md border-0 font-semibold bg-slate-400 uppercase">
+              Play
+            </button>
+            <button className="block text-sm text-white mr-4 py-2 px-4 rounded-md border-0 font-semibold bg-slate-400 uppercase">
+              Add To My List
+            </button>
+          </div>
+
+          <p className="uppercase font-bold text-xl text-white text-start">
+            RATING: 8.6/10
+          </p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
