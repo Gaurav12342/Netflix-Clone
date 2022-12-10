8914cb03f13e5f7ae92747a0d63b06f305595d8a
Gaurav12342 <gauravpendharkar2@gmail.com>
2022-12-10 13:04:59 +0530

    Added changes

--- src/component/Carousel/index.tsx
+++ src/component/Carousel/index.tsx
@@ -1,99 +1,125 @@
-
-import { FC, useState } from 'react'
+import { FC, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Grid, Dialog, DialogContent, Paper, PaperProps } from '@mui/material';
-import '../../App.css';
-import Draggable from 'react-draggable';
-import ReactPlayer from 'react-player'
-import axios from 'utils/AxiosInterceptor';
+import { Grid, Dialog, DialogContent, Paper, PaperProps } from "@mui/material";
+import "../../App.css";
+import Draggable from "react-draggable";
+import ReactPlayer from "react-player";
+import axios from "utils/AxiosInterceptor";
 
+interface IData {
+  page?: number;
+  results?: string[];
+  total_pages?: number;
+  total_results?: number;
+}
 interface IProps {
-    data?: any,
-    name?: string
-};
+  data?: IData;
+  name?: string;
+}
+
+interface IMovie{
+    
+}
+
 
 const PaperComponent = (props: PaperProps) => {
-    return (
-        <Draggable
-            handle="#draggable-dialog-title"
-            cancel={'[class*="MuiDialogContent-root"]'}
-        >
-            <Paper {...props} />
-        </Draggable>
-    );
-}
-
+  return (
+    <Draggable
+      handle="#draggable-dialog-title"
+      cancel={'[class*="MuiDialogContent-root"]'}
+    >
+      <Paper {...props} />
+    </Draggable>
+  );
+};
 
 const Carousel: FC<IProps> = (props) => {
-    const { data, name } = props;
-    const [open, setOpen] = useState(false);
-    const [videoUrl, setVideoUrl] = useState("");
+  const { data, name } = props;
+  const [open, setOpen] = useState(false);
+  const [videoUrl, setVideoUrl] = useState("");
 
-    const handleDialog = (value: any) => {
-        setOpen(false);
-        axios.get(`tv/${value?.id}/videos`).then((data: any) => {
-            if (data?.status === 200) {
-                const url = `https://www.youtube.com/watch?v=${data?.data?.results[0]?.key}`;
-                setVideoUrl(url);
-                setOpen(true);
-            }
-        }).catch()
-    };
+  const handleDialog = (value: any) => {
+    console.log("Values", value);
 
-    const handleClose = () => {
-        setOpen(false);
-        setVideoUrl("");
-    };
+    setOpen(false);
+    axios
+      .get(`movie/${value?.id}/videos`)
+      // .get(`tv/${value?.id}/videos`)
+      .then((data: any) => {
+        if (data?.status === 200) {
+          const url = `https://www.youtube.com/watch?v=${data?.data?.results[0]?.key}`;
+          setVideoUrl(url);
+          setOpen(true);
+        }
+      })
+      .catch();
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+    setVideoUrl("");
+  };
 
-    const settings = {
-        centerMode: false,
-        infinite: false,
-        centerPadding: "60px",
-        slidesToShow: 5,
-        speed: 500,
-        dots: true,
-        arrows: true,
-        focusOnSelect: true
-    };
+  const settings = {
+    centerMode: false,
+    infinite: false,
+    centerPadding: "60px",
+    slidesToShow: 5,
+    speed: 500,
+    dots: true,
+    arrows: true,
+    focusOnSelect: true,
+  };
 
-    return (
-        <>
-            <div className='px-6 pb-8'>
-                <Grid className="pb-5">
-                    <label className='uppercase font-bold text-3xl text-white'>
-                        {name}
-                    </label>
-                </Grid>
-                <Slider {...settings}>
-                    {data?.results?.map((dd: any) => {
-                        return (
-                            <div className='w-8'>
-                                <div className='bg-slate-400 w-56 h-73 cursor-pointer' onClick={() => handleDialog(dd)}>
-                                    <img src={`https://image.tmdb.org/t/p/w342/${dd?.poster_path}`} alt="movie-image" />
-                                </div>
-                            </div>
-                        )
-                    })}
-                </Slider>
-            </div>
+  return (
+    <>
+      <div className="px-6 pb-8">
+        <Grid className="pb-5">
+          <label className="uppercase font-bold text-3xl text-white">
+            {name}
+          </label>
+        </Grid>
+        <Slider {...settings}>
+          {data?.results?.map((dd: any) => {
+            return (
+              <div className="w-8">
+                <div
+                  className="bg-slate-400 w-56 h-73 cursor-pointer"
+                  onClick={() => handleDialog(dd)}
+                >
+                  <img
+                    src={`https://image.tmdb.org/t/p/w342/${dd?.poster_path}`}
+                    alt="movie-image"
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
 
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                PaperComponent={PaperComponent}
-                fullWidth={true}
-                maxWidth={'md'}
-                aria-labelledby="draggable-dialog-title"
-            >
-                <DialogContent>
-                    <ReactPlayer muted={true} width={"840px"} playing={true} controls={true} url={videoUrl} />
-                </DialogContent>
-            </Dialog>
-        </>
-    )
-}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperComponent={PaperComponent}
+        fullWidth={true}
+        maxWidth={"md"}
+        aria-labelledby="draggable-dialog-title"
+      >
+        <DialogContent>
+          <ReactPlayer
+            muted={true}
+            width={"840px"}
+            playing={true}
+            controls={true}
+            url={videoUrl}
+          />
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
 
-export default Carousel
+export default Carousel;
--- src/component/Layout/Header.tsx
+++ src/component/Layout/Header.tsx
@@ -1,130 +1,151 @@
-import { useState } from 'react';
-import { MenuItem, Tooltip, Avatar, Container, Menu, IconButton, AppBar, Toolbar, Typography, CssBaseline, useScrollTrigger, Slide, Box } from '@mui/material';
-import AdbIcon from '@mui/icons-material/Adb';
-import MenuIcon from '@mui/icons-material/Menu';
+import { FC, useState } from "react";
+import {
+  MenuItem,
+  Tooltip,
+  Avatar,
+  Container,
+  Menu,
+  IconButton,
+  AppBar,
+  Toolbar,
+  Typography,
+  CssBaseline,
+  useScrollTrigger,
+  Slide,
+  Box,
+} from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ["Products", "Pricing", "Blog"];
 
 interface Props {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    window?: () => Window;
-    children: React.ReactElement;
+  window?: () => Window;
+  children: React.ReactElement;
 }
 
 const HideOnScroll = (props: Props) => {
-    const { children, window } = props;
-    const trigger = useScrollTrigger({
-        target: window ? window() : undefined,
-    });
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
 
-    return (
-        <Slide appear={false} direction="down" in={!trigger}>
-            {children}
-        </Slide>
-    );
-}
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+};
 
-const Header = (props: any) => {
-    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
-    const [_, setAnchorElUser] = useState<null | HTMLElement>(null);
+const Header: FC = (props) => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [_, setAnchorElUser] = useState<null | HTMLElement>(null);
+  
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  return (
+    <>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar sx={{ backgroundColor: "#272727" }}>
+          <Container maxWidth="xl">
+            <Toolbar>
+              <img
+                className="w-24"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+                alt=""
+              />
+              <img
+                className="w-24 ml-3 h-10"
+                src="	https://reactflix.anthonyperuso.me/tmdb-logo.svg"
+                alt=""
+              />
 
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href=""
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                LOGO
+              </Typography>
+              <Box
+                sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+              ></Box>
 
-    return (
-        <>
-            <CssBaseline />
-            <HideOnScroll {...props}>
-                <AppBar sx={{ backgroundColor: '#272727' }}>
-                    <Container maxWidth="xl">
-                        <Toolbar>
-                            <img className='w-24' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="" />
-                            <img className='w-24 ml-3 h-10' src="	https://reactflix.anthonyperuso.me/tmdb-logo.svg" alt="" />
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Gaurav Sali"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+    </>
+  );
+};
 
-                            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                                <IconButton
-                                    size="large"
-                                    aria-label="account of current user"
-                                    aria-controls="menu-appbar"
-                                    aria-haspopup="true"
-                                    onClick={handleOpenNavMenu}
-                                    color="inherit"
-                                >
-                                    <MenuIcon />
-                                </IconButton>
-                                <Menu
-                                    id="menu-appbar"
-                                    anchorEl={anchorElNav}
-                                    anchorOrigin={{
-                                        vertical: 'bottom',
-                                        horizontal: 'left',
-                                    }}
-                                    keepMounted
-                                    transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'left',
-                                    }}
-                                    open={Boolean(anchorElNav)}
-                                    onClose={handleCloseNavMenu}
-                                    sx={{
-                                        display: { xs: 'block', md: 'none' },
-                                    }}
-                                >
-                                    {pages.map((page) => (
-                                        <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                            <Typography textAlign="center">{page}</Typography>
-                                        </MenuItem>
-                                    ))}
-                                </Menu>
-                            </Box>
-                            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                            <Typography
-                                variant="h5"
-                                noWrap
-                                component="a"
-                                href=""
-                                sx={{
-                                    mr: 2,
-                                    display: { xs: 'flex', md: 'none' },
-                                    flexGrow: 1,
-                                    fontFamily: 'monospace',
-                                    fontWeight: 700,
-                                    letterSpacing: '.3rem',
-                                    color: 'inherit',
-                                    textDecoration: 'none',
-                                }}
-                            >
-                                LOGO
-                            </Typography>
-                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                            </Box>
-
-                            <Box sx={{ flexGrow: 0 }}>
-                                <Tooltip title="Open settings">
-                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                        <Avatar alt="Gaurav Sali" src="/static/images/avatar/2.jpg" />
-                                    </IconButton>
-                                </Tooltip>
-                            </Box>
-                        </Toolbar>
-                    </Container>
-                </AppBar>
-            </HideOnScroll>
-            <Toolbar />
-        </>
-    );
-}
-
-export default Header
+export default Header;
