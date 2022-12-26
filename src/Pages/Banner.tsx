76efbe76ab551c8fa993440135d7d340e67984d1
Gaurav12342 <gauravpendharkar2@gmail.com>
2022-12-26 23:42:24 +0530

    Added basics linear gadient

--- src/Pages/Banner.tsx
+++ src/Pages/Banner.tsx
@@ -102,7 +102,7 @@ const Banner: FC = () => {
   return (
     <div>
       <div className="text-center text-yellow-600 mb-10 relative">
-        <img className="relative w-1/1" src={bannerImage} alt="Freedom Blog" />
+        <img className="relative w-1/1 after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-r from-slate-100 to-blue-500" src={bannerImage} alt="Freedom Blog" />
         <div className="absolute top-20 left-10 flex flex-col items-start space-y-8">
           <label className="font-bold text-5xl md:text-4xl text-white w-2/5 text-start md:w-[40rem]">
             {bannerData?.name}
